f39177dcf35426ab3a54a3b17a05dd8b064c544b
vishalBindal <vishalbindal.06@gmail.com>
2021-03-28 20:33:43 +0530

    Add lecture slides

--- project/main.py
+++ project/main.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user
 from werkzeug.utils import secure_filename
 from . import db
-from .models import Course, User, User_Course, Lecture
+from .models import Course, User, User_Course, Lecture, Slide
 import os
 
 main = Blueprint('main', __name__)
@@ -151,13 +151,106 @@ def newlecture_post(code):
         num_lectures += 1
     lecture_no = num_lectures+1
 
-    ppt.save(os.path.join('files', f'course-{course.id}-lecture-{lecture_no}.pdf'))
-    video.save(os.path.join('files', f'course-{course.id}-lecture-{lecture_no}.mp4'))
+    if ppt.filename == '' or video.filename == '':
+        flash('Please select both files')
+        return redirect(url_for(main.newlecture))
+    
+    try:
+        ppt_ext = secure_filename(ppt.filename).split('.')[1]
+        video_ext = secure_filename(video.filename).split('.')[1]
+        if ppt_ext != 'pdf' or video_ext != 'mp4':
+            flash('Only pdf and mp4 files supported at the moment.')
+            return redirect(url_for(main.newlecture))
+    except:
+        flash('Error parsing extensions of files. Please check the files.')
+        return redirect(url_for(main.newlecture))
+
+    ppt_path = os.path.join('files', f'course-{course.id}-lecture-{lecture_no}.pdf')
+    video_path = os.path.join('files', f'course-{course.id}-lecture-{lecture_no}.mp4')
+    ppt.save(ppt_path)
+    video.save(video_path)
 
     lecture = Lecture(name=name, lecture_no=lecture_no, course_id=course.id)
     db.session.add(lecture)
     db.session.commit()
 
-    # Processing here for slides
+    slides = []
+    # slides = get_slides(ppt_path, video_path)
     
+    for slide in slides:
+        new_slide = Slide(
+            lecture_id = lecture.id, 
+            slide_no = slide['slide_no'],
+            subtitles = slide['subtitles'],
+            image_path = slide['image_path'],
+            audio_path = slide['audio_path']
+        )
+        db.session.add(new_slide)
+        db.session.commit()
+
     return redirect(url_for('main.course', code=code))
+
+@main.route('/slides/<slide_id>')
+@login_required
+def slides(slide_id):
+    slide = Slide.query.filter_by(id=slide_id).first()
+    if not slide:
+        return render_template('404.html')
+    
+    lecture = Lecture.query.filter_by(id=slide.lecture_id).first()
+    if not lecture:
+        return render_template('404.html')
+
+    course = Course.query.filter_by(id=lecture.course_id).first()
+    if not course:
+        return render_template('404.html')
+    
+    if current_user.is_instructor:
+        if course.instructor_id != current_user.id:
+            return 'Invalid access'
+    else:
+        user_course = User_Course.query.filter_by(course_id=course.id,user_id=current_user.id).first()
+        if not user_course:
+            return 'Invalid access'
+    
+    all_slides = Slide.query.filter_by(lecture_id=lecture.id)
+    total_slides = 0
+    for slide in all_slides:
+        total_slides += 1
+
+    previous_slide_id, next_slide_id = None, None
+    previous_slide = Slide.query.filter_by(slide_no=slide.slide_no-1).first()
+    next_slide = Slide.query.filter_by(slide_no=slide.slide_no+1).first()
+    if previous_slide:
+        previous_slide_id = previous_slide.id
+    if next_slide:
+        next_slide_id = next_slide.id
+
+    return render_template('slide_page.html', slide=slide, course_name=course.name, lecture_name=lecture.name, total_slides=total_slides, previous_slide_id=previous_slide_id, next_slide_id=next_slide_id)
+
+@main.route('/lecture/<lecture_id>')
+@login_required
+def lecture(lecture_id):
+    slide = Slide.query.filter_by(lecture_id=lecture_id, slide_no=1).first()
+    if not slide:
+        flash('No slides found')
+        return render_template('404.html')
+    
+    lecture = Lecture.query.filter_by(id=slide.lecture_id).first()
+    if not lecture:
+        return render_template('404.html')
+
+    course = Course.query.filter_by(id=lecture.course_id).first()
+    if not course:
+        return render_template('404.html')
+    
+    if current_user.is_instructor:
+        if course.instructor_id != current_user.id:
+            return 'Invalid access'
+    else:
+        user_course = User_Course.query.filter_by(course_id=course.id,user_id=current_user.id).first()
+        if not user_course:
+            return 'Invalid access'
+
+    return redirect(url_for('main.slides', slide_id=slide.id))
+    
