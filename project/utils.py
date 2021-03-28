9195ac743b3f624b0c988605b51b0abc2e535685
Vishal Singh <tmibvishal@gmail.com>
2021-03-28 21:14:15 +0530

    add diff image matching

---
+++ project/utils.py
@@ -0,0 +1,130 @@
+import os
+import cv2
+import numpy as np
+from pdf2image import convert_from_path
+from pdf2image.generators import threadsafe
+from skimage.metrics import structural_similarity as ssim
+from skimage import exposure
+import pytesseract
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def convert_pdf_to_images(pdf_file, out_dir=None, save_pages=False):
+    pages = convert_from_path(pdf_file, 500)
+    # print(type(pages[0]))
+    # Saving the pages
+    if save_pages:
+        i = 0
+        os.makedirs(os.path.join(out_dir), exist_ok=True)
+        for page in pages:
+            page.save(os.path.join(out_dir, "out" + str(i) + ".jpg"), "JPEG")
+            i += 1
+    return pages
+
+# pdf_file = './data/sample.pdf'
+# pages = convert_pdf_to_images(pdf_file, "data/saved", True)
+
+def fraction_match_images(image_a, image_b):
+	# compute the structural similarity
+	# returns 0 to 1 depending on percentage match of images
+    # s = ssim(image_a, image_b, multichannel = True)
+    # s = SIFT_match(imageA, imageB)
+	# print("SSIM: %.2f" % (s))
+    return ocr_matching(image_a, image_b)
+
+def ocr_matching(image_a, image_b):
+    corpus = (pytesseract.image_to_string(image_a), pytesseract.image_to_string(image_b))
+    print(corpus[0])
+    print(corpus[1])
+    vect = TfidfVectorizer(min_df=1, stop_words="english")
+    tfidf = vect.fit_transform(corpus)
+    pairwise_similarity = tfidf * tfidf.T
+    return pairwise_similarity.toarray()[0][1]                                                                                                                                                                                                            
+
+
+def ssim_matching(imageA, imageB):
+    s = ssim(imageA, imageB, multichannel = True)
+    return s
+
+def match_using_histogram(imageA, imageB):
+    multi = True if imageA.shape[-1] > 1 else False
+    matched = exposure.match_histograms(imageA, imageB, multichannel=multi)
+    return matched
+
+# From our previous project - ImageLocalisation - DISA 2019 (Vishal Singh, Vishal Bindal, Sushant Sondhi)
+def SIFT_match(img1, img2, hessianThreshold: int = 400, ratio_thresh: float = 0.7, symmetry_match: bool = True, show_image: bool = False):
+    """Give fraction match between 2 images using SURF and FLANN
+    Parameters
+    ----------
+    img1 : Open CV image format,
+    img2 : Open CV image format,
+    hessianThreshold: Number of ORB points to consider in a image,
+    ratio_thresh: (b/w 0 to 1) lower the number more serious the matching
+    symmetry_match:
+    Returns
+    -------
+    float,
+        returns a number from 0 to 1 depending on percentage match and returns -1 if any of the parameter is None
+    """
+    if img1 is None or img2 is None:
+        raise Exception("img1 or img2 can't be none")
+
+    if ratio_thresh > 1 or ratio_thresh < 0:
+        raise Exception("ratio_thresh not between 0 to 1")
+
+    detector = cv2.SIFT().create(hessianThreshold)
+    keypoints1, descriptors1 = detector.detectAndCompute(img1, None)
+    keypoints2, descriptors2 = detector.detectAndCompute(img2, None)
+
+    a1 = len(keypoints1)
+    b1 = len(keypoints2)
+
+    if a1 < 2 or b1 < 2:
+        return 0
+
+    matcher = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_FLANNBASED)
+    knn_matches = matcher.knnMatch(descriptors1, descriptors2, 2)
+    good_matches = []
+    for m, n in knn_matches:
+        if m.distance < ratio_thresh * n.distance:
+            good_matches.append(m)
+    c1 = len(good_matches)
+
+    if (show_image):
+        img3 = np.empty((max(img1.shape[0], img2.shape[0]), img1.shape[1] + img2.shape[1], 3), dtype=np.uint8)
+        cv2.drawMatches(
+            img1, keypoints1, img2, keypoints2, good_matches, outImg=img3, matchColor=None, flags=2)
+        if img3 is not None:
+            cv2.namedWindow("matches", cv2.WINDOW_NORMAL)
+            cv2.resizeWindow("matches", 1600, 1600)
+            cv2.imshow("matches", img3)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+    if symmetry_match:
+        knn_matches = matcher.knnMatch(descriptors2, descriptors1, 2)
+        good_matches = []
+        for m, n in knn_matches:
+            if m.distance < ratio_thresh * n.distance:
+                good_matches.append(m)
+        c2 = len(good_matches)
+
+        if (show_image):
+            img3 = np.empty((max(img1.shape[0], img2.shape[0]), img1.shape[1] + img2.shape[1], 3), dtype=np.uint8)
+            cv2.drawMatches(
+                img2, keypoints2, img1, keypoints1, good_matches, outImg=img3, matchColor=None, flags=2)
+            if img3 is not None:
+                cv2.namedWindow("matches", cv2.WINDOW_NORMAL)
+                cv2.resizeWindow("matches", 1600, 1600)
+                cv2.imshow("matches", img3)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
+
+        # print(a1,"a1", b1,"b1", c1, "c1", c2, "c2")
+        fraction = (c1 + c2) / (a1 + b1)
+        return fraction
+
+    fraction = (2.0 * c1) / (a1 + b1)
+    if fraction > 1:
+        fraction = 1
+    # fraction can be greater than one in blur images because we are multiplying fraction with 2
+    return fraction
--- project/video_utils.py
+++ project/video_utils.py
@@ -2,11 +2,14 @@
 # Check this link for more info https://medium.com/@haydenfaulkner/extracting-frames-fast-from-a-video-using-opencv-and-python-73b9b7dc9661
 # Credits to Hayden Faulkner
 
-import cv2  # still used to save images out
 import os
+import cv2  # still used to save images out
 import numpy as np
 from decord import VideoReader
 from decord import cpu, gpu
+import utils
+
+PROCESSED_FILES_DIR = "project/testing/"
 
 
 def extract_frames(video_path, frames_dir, overwrite=False, start=-1, end=-1, every=30):
@@ -21,16 +24,20 @@ def extract_frames(video_path, frames_dir, overwrite=False, start=-1, end=-1, ev
     :return: count of images saved
     """
 
-    video_path = os.path.normpath(video_path)  # make the paths OS (Windows) compatible
-    frames_dir = os.path.normpath(frames_dir)  # make the paths OS (Windows) compatible
+    # make the paths OS (Windows) compatible
+    video_path = os.path.normpath(video_path)
+    # make the paths OS (Windows) compatible
+    frames_dir = os.path.normpath(frames_dir)
 
-    video_dir, video_filename = os.path.split(video_path)  # get the video path and filename from the path
+    # get the video path and filename from the path
+    video_dir, video_filename = os.path.split(video_path)
 
     assert os.path.exists(video_path)  # assert the video file exists
 
     # load the VideoReader
-    vr = VideoReader(video_path, ctx=cpu(0))  # can set to cpu or gpu .. ctx=gpu(0)
-                     
+    # can set to cpu or gpu .. ctx=gpu(0)
+    vr = VideoReader(video_path, ctx=cpu(0))
+
     if start < 0:  # if start isn't specified lets assume 0
         start = 0
     if end < 0:  # if end isn't specified assume the end of the video
@@ -39,24 +46,33 @@ def extract_frames(video_path, frames_dir, overwrite=False, start=-1, end=-1, ev
     frames_list = list(range(start, end, every))
     saved_count = 0
 
-    if every > 25 and len(frames_list) < 1000:  # this is faster for every > 25 frames and can fit in memory
+    # this is faster for every > 25 frames and can fit in memory
+    if every > 25 and len(frames_list) < 1000:
         frames = vr.get_batch(frames_list).asnumpy()
 
-        for index, frame in zip(frames_list, frames):  # lets loop through the frames until the end
-            save_path = os.path.join(frames_dir, video_filename, "{:010d}.jpg".format(index))  # create the save path
+        # lets loop through the frames until the end
+        for index, frame in zip(frames_list, frames):
+            save_path = os.path.join(frames_dir, "{:010d}.jpg".format(
+                index))  # create the save path
             print(f"frame {index} saved")
-            if not os.path.exists(save_path) or overwrite:  # if it doesn't exist or we want to overwrite anyways
-                cv2.imwrite(save_path, cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))  # save the extracted image
+            # if it doesn't exist or we want to overwrite anyways
+            if not os.path.exists(save_path) or overwrite:
+                # save the extracted image
+                cv2.imwrite(save_path, cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
                 saved_count += 1  # increment our counter by one
 
     else:  # this is faster for every <25 and consumes small memory
         for index in range(start, end):  # lets loop through the frames until the end
             if index % every == 0:  # if this is a frame we want to write out based on the 'every' argument
                 frame = vr[index]  # read an image from the capture
-                save_path = os.path.join(frames_dir, video_filename, "{:010d}.jpg".format(index))  # create the save path
+                save_path = os.path.join(frames_dir, "{:010d}.jpg".format(
+                    index))  # create the save path
                 print(f"frame {index} saved")
-                if not os.path.exists(save_path) or overwrite:  # if it doesn't exist or we want to overwrite anyways
-                    cv2.imwrite(save_path, cv2.cvtColor(frame.asnumpy(), cv2.COLOR_RGB2BGR))  # save the extracted image
+                # if it doesn't exist or we want to overwrite anyways
+                if not os.path.exists(save_path) or overwrite:
+                    # save the extracted image
+                    cv2.imwrite(save_path, cv2.cvtColor(
+                        frame.asnumpy(), cv2.COLOR_RGB2BGR))
                     saved_count += 1  # increment our counter by one
 
     return saved_count  # and return the count of the images we saved
@@ -72,21 +88,95 @@ def video_to_frames(video_path, frames_dir, overwrite=False, every=1):
     :return: path to the directory where the frames were saved, or None if fails
     """
 
-    video_path = os.path.normpath(video_path)  # make the paths OS (Windows) compatible
-    frames_dir = os.path.normpath(frames_dir)  # make the paths OS (Windows) compatible
+    # make the paths OS (Windows) compatible
+    video_path = os.path.normpath(video_path)
+    # make the paths OS (Windows) compatible
+    frames_dir = os.path.normpath(frames_dir)
 
-    video_dir, video_filename = os.path.split(video_path)  # get the video path and filename from the path
+    # get the video path and filename from the path
+    video_dir, video_filename = os.path.split(video_path)
 
     # make directory to save frames, its a sub dir in the frames_dir with the video name
-    os.makedirs(os.path.join(frames_dir, video_filename), exist_ok=True)
-    
-    print("Extracting frames from {}".format(video_filename))
-    
-    extract_frames(video_path, frames_dir, every=every)  # let's now extract the frames
+    os.makedirs(os.path.join(frames_dir), exist_ok=True)
 
-    return os.path.join(frames_dir, video_filename)  # when done return the directory containing the frames
+    print("Extracting frames from {}".format(video_filename))
+
+    # let's now extract the frames
+    extract_frames(video_path, frames_dir, every=every)
+
+    # when done return the directory containing the frames
+    return os.path.join(frames_dir)
+
+
+def seperate_into_slides(pdf_file_path, video_file_path, user_id, project_id):
+    pdf_out_dir = os.path.join(PROCESSED_FILES_DIR, user_id, project_id, "pdf_pages")
+    # utils.convert_pdf_to_images(pdf_file_path, out_dir=pdf_out_dir, save_pages=True)
+    video_frames_dir = os.path.join(PROCESSED_FILES_DIR, user_id, project_id, "video_frames")
+    # video_to_frames(video_path=video_file_path, frames_dir=video_frames_dir, overwrite=True, every=60)
+
+    contrast = cv2.imread("images/jp_gates_contrast.png")
+    pdf_frames = []
+    for pdf_file_name in os.listdir(pdf_out_dir):
+        if pdf_file_name.endswith(".jpg"):
+            img = cv2.imread(os.path.join(pdf_out_dir, pdf_file_name))
+            img = cv2.resize(img, (256, 256))
+            pdf_frames.append(img)
+
+    video_frames = []
+    for video_frame_filename in os.listdir(video_frames_dir):
+        if video_frame_filename.endswith(".jpg"):
+            img = cv2.imread(os.path.join(video_frames_dir, video_frame_filename))
+            img = cv2.resize(img, (256, 256))
+            video_frames.append((int(os.path.splitext(video_frame_filename)[0]), img))
+
+    video_frames.sort()
+
+    # {
+    # slide_no:,
+    # subtitles:,
+    # image_path:,
+    # audio_path:
+    # }
+
+    assert len(pdf_frames) > 0
+    assert len(video_frames) > 0
+
+    for slide_no in range(0, len(pdf_frames)):
+        pdf_frame = pdf_frames[slide_no]
+        all_comparision = []
+        for frame_index, video_frame in video_frames:
+            s = utils.fraction_match_images(pdf_frame, video_frame)
+            all_comparision.append((frame_index, s))
+            print(f"slide_no {slide_no}, frame_index {frame_index}, ssim {s}")
+        
+        maxs = -1
+        for _, s in all_comparision:
+            s = max(maxs, s)
+        
+        best_matched = (-1, -1)
+        for i in range(0, len(all_comparision)):
+            frame_index, s = all_comparision[i]
+            if s >= 0.96*(maxs):
+                best_matched = (frame_index, s)
+                break
+            last_s = s
+
+        assert best_matched[0] != -1
+
+        # frame_index, s = all_comparision[0]
+        # best_matched = [frame_index, s]
+        # last_s = s
+        # for i in range(1, len(all_comparision)):
+        #     frame_index, s = all_comparision[i]
+        #     change = s - last_s
+        #     if best_matched[1] < change + s:
+        #         best_matched = (frame_index, change + s)
+        #     last_s = s
+        
+        print(f"slide_no {slide_no}, video_frame {best_matched[0]}")
 
 
 if __name__ == '__main__':
     # test it
-    video_to_frames(video_path='project/testing/lecture.mp4', frames_dir='project/testing/extracted_frames', overwrite=True, every=60)
+    # video_to_frames(video_path='project/testing/lecture.mp4', frames_dir='project/testing/extracted_frames', overwrite=True, every=60)
+    seperate_into_slides("project/testing/lecture.pdf", "project/testing/lecture.mp4", "0", "0")
